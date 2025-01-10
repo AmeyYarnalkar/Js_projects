75cdf389b3c9dec54c98aee7dbf069da525ef19b
Amey Yarnakar <162596043+AmeyYarnalkar@users.noreply.github.com>
2025-01-10 08:08:25 +0530

    Add files via upload

---
+++ Pig_Game/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//importing the images
+
+
+// getting access to the elements
+
+// access to players
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
+// access to  cuurent scores
+const currentScore1 = document.getElementById('current--0');
+const currentScore2 = document.getElementById('current--1');
+
+//access to total scores
+const totalScore1 = document.getElementById('score--0');
+const totalScore2 = document.getElementById('score--1');
+
+//access to dice
+const dice = document.querySelector('.dice');
+
+//access to buttons
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
